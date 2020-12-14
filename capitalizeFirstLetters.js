481556b4bf84d73f9ff142a65fb4ba92136aa597
Rooarii <rooarii.manuel@gmail.com>
2020-12-14 11:22:40 +0100

    capitalizeFirstLetters feature ok

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -1,7 +1,28 @@
-const assert = require('assert')
+const assert = require('assert');
 
 // Type your code here
+const capitalizeFirstLetters =(input)=>{
+  // return input.length && !input.includes(' ') > 0 ? input[0].toUpperCase() + input.slice(1): '';
 
+  // first code version
+  let output = '';
+  if (input.length >0 && !input.includes(' '))
+    {
+      output = input[0].toUpperCase() + input.slice(1);
+    }
+  else if (input.length >0 && input.includes(' '))
+    {
+      outputSplit = input.split(' ').map(e =>(
+        e[0].toUpperCase()+ e.slice(1))
+      );
+      output = outputSplit.join(' ');
+    }
+  else 
+    {
+      output ='';
+    }
+  return output;
+}
 // ************* tests
 
 // test1: check that capitalizeFirstLetters is a function
