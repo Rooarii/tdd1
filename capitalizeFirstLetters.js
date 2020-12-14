5c65c884e22949b4f9e0eb8e070766d583bb00db
Rooarii <rooarii.manuel@gmail.com>
2020-12-14 11:16:50 +0100

    tests defined

---
+++ capitalizeFirstLetters.js
@@ -0,0 +1,26 @@
+const assert = require('assert')
+
+// Type your code here
+
+// ************* tests
+
+// test1: check that capitalizeFirstLetters is a function
+assert.strictEqual(typeof capitalizeFirstLetters, 'function');
+
+// test2: check that capitalizeFirstLetters takes one argument
+assert.strictEqual(capitalizeFirstLetters.length, 1)
+
+// // test3: check that capitalizeFirstLetters argument is a string
+// assert.strictEqual(typeof input, 'string');
+
+// test4: Check that capitalizeFirstLetters transforms javaScript correctly
+assert.strictEqual(capitalizeFirstLetters('javaScript'), 'JavaScript');
+
+// test5: Check that it works for a 1-character string
+assert.strictEqual(capitalizeFirstLetters('z'), 'Z');
+
+// test6: Check that it works for i am learning TDD
+assert.strictEqual(capitalizeFirstLetters('i am learning TDD'), 'I Am Learning TDD');
+
+// test7: Check that it works for an empty string
+assert.strictEqual(capitalizeFirstLetters(''), '');
